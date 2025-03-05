18883d6e86cbb8fa68723523a5e8fa7bb12a808e
Rohit1164 <mrrohit1164@gmail.com>
2025-03-05 10:41:03 +0530

    some dependencies install

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const path = require('path');
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser')
-const blog= require('./models/blog')
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const blog = require("./models/blog");
 
-const useRouter = require('./Router/user');
-const blogRouter = require('./Router/blog');
-const { checkForAuthenticationCookies } = require('./middleware/authentication');
+const useRouter = require("./Router/user");
+const blogRouter = require("./Router/blog");
+const {
+  checkForAuthenticationCookies,
+} = require("./middleware/authentication");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -16,29 +18,29 @@ const PORT = process.env.PORT;
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log('MongoDB connected'))
+  .then(() => console.log("MongoDB connected"))
   .catch((error) => console.error("Error connecting to MongoDB:", error));
 
 // Set up view engine
-app.set('view engine', 'ejs');
-app.set('views', path.resolve('./view'));
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./view"));
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser())
-app.use(checkForAuthenticationCookies("token"))
-app.use(express.static(path.resolve('./public')))
+app.use(cookieParser());
+app.use(checkForAuthenticationCookies("token"));
+app.use(express.static(path.resolve("./public")));
 
 // Routes
-app.get("/",async(req, res) => {
-  const allblogs=await blog.find({})
+app.get("/", async (req, res) => {
+  const allblogs = await blog.find({});
   res.render("home", {
     user: req.user || null,
     blogs: allblogs || null,
   });
 });
 
-app.use('/user', useRouter);
-app.use('/blog', blogRouter);
+app.use("/user", useRouter);
+app.use("/blog", blogRouter);
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
